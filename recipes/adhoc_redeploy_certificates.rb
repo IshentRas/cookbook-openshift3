8bcbce812ac48b7335b23fa88e02d2dde89ac88f
William Burton <william17.burton@gmail.com>
2018-03-22 13:14:43 +0000

    Update name

--- recipes/adhoc_redeploy_certificates.rb
+++ recipes/adhoc_redeploy_certificates.rb
@@ -1,5 +1,5 @@
 #
-# Cookbook Name:: cookbook-openshift3
+# Cookbook Name:: is_apaas_openshift_cookbook
 # Recipe:: adhoc_redeploy_certificates
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
